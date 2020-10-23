be19dfe9093cee672ca1187a4c16720ffe715ce2
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2020-10-23 16:29:06 -0300

    Exercise: ordenation of numbers

---
+++ exercises/Nunber_Ordene/description.md
@@ -0,0 +1,4 @@
+Make an algorithm should organize the sequence of numbers in order to grow:
+
+input: " 1, 4, 88, 55, 3,..."
+output: " 1, 3, 4, 55, 88,..."
---
+++ exercises/Nunber_Ordene/nunber_Ordene.js
@@ -0,0 +1,10 @@
+var arr = [1, 4, 88, 55, 3];
+
+console.log('Array original:', arr);
+
+ arr.sort(function(a, b) {
+     return a - b;
+ });
+
+ console.log('Array ordenado:', arr);
+
