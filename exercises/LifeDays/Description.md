5d8ec76fff55b292f2945f57ed4b4bc234c02af4
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2020-10-27 22:10:59 -0300

    Incomplete exercise

---
+++ exercises/LifeDays/Description.md
@@ -0,0 +1,1 @@
+Make an algorithm that reads a person's age expressed in years, months and days and shows it expressed only in days.
---
+++ exercises/LifeDays/Resolution.js
@@ -0,0 +1,11 @@
+const day = 28
+const month = 12
+const year = 1991
+
+var yearToString = parseInt(year, 10);
+
+let a = leap => {
+    return typeof leap
+}
+
+console.log(a(yearToString))
