50f8ca82cd017846228d0b39312ebbf37b455d44
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2020-11-03 20:45:16 -0300

    Exercise LifeDays

--- exercises/LifeDays/Resolution.js
+++ exercises/LifeDays/Resolution.js
@@ -1,11 +1,69 @@
 const day = 28
 const month = 12
 const year = 1991
+let data = new Date()
 
-var yearToString = parseInt(year, 10);
+let verify = leap => {
 
-let a = leap => {
-    return typeof leap
+  if (leap % 4 == 0){
+    if (leap % 100 != 0){
+        return true
+    }
+  }
+
+  if (leap % 100 != 0){
+    return true
+  }
+
+  if (leap % 400 == 0){
+    return true
+  }
+  return false 
+
 }
 
-console.log(a(yearToString))
+let countTheDaysOfYears = count => {
+    let day = 0
+    for (let i = count; i < data.getFullYear(); i++) {
+        if(verify(year)){
+            day = day + 366
+        }
+        else{
+            day = day + 365
+        }   
+    }
+    return(day)
+}
+
+let quantityDaysInThisMont = (mes, ano) => {
+    var data = new Date(ano, mes, 0);
+    return data.getDate();
+}
+
+let countTheDayOfMonts = count => {
+    let day = 0
+    for (let i = 1; i < data.getMonth(); i++) {
+
+       day = day + quantityDaysInThisMont(i, data.getFullYear())
+
+    }
+    for (let i = 1; i <= count; i++) {
+
+        day = day - quantityDaysInThisMont(i, year)
+ 
+    }
+    
+    return day
+} 
+
+let countLastDays = (day) => {
+    return data.getDate() + day 
+    
+}
+
+console.log(countTheDaysOfYears(year)+ countTheDayOfMonts(month) + countLastDays(day) - data.getMonth() )
+
+
+
+
+
